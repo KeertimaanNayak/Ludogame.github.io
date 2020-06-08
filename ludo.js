14e39000d84686af6205288969a16d9303f27e53
KeertimaanNayak <keertimaan_nayak@rediffmail.com>
2020-06-08 17:27:14 +0530

    Update ludo.js

--- ludo.js
+++ ludo.js
@@ -8,8 +8,21 @@ var currpawn = "";
 var allcolor = ["red", "blue", "green", "yellow"];
 var pawnOut = {red:0,blue:0,green:0,yellow:0}
 function randomNum() {
+    var value=window.prompt("Enter number between 1 and 6 or enter space or nothing to roll dice");
+    if(!clicked){
+        if(value!=null || value!="")
+        {
+            num=Number(value);
+        }
+    }
     if (!clicked) {
+        if(value=="null"|| value==""){
         num= Math.floor((Math.random() * 6) + 1);
+    }
+        var dice = document.getElementById('dice');
+        dice.style.backgroundImage = "url( " + num + ".jpg)";
+        clicked = true;
+    }
         var dice = document.getElementById('dice');
         dice.style.backgroundImage = "url( " + num + ".jpg)";
         clicked = true;
